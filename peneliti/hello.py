3375974ae0f27f47a660cea510459175ec4bf893
fadilaa <dhildhila12@gmail.com>
2018-05-31 21:36:19 +0700

    Fadila lagi pak hehe

--- peneliti/hello.py
+++ peneliti/hello.py
@@ -16,3 +16,11 @@ shark = "Sammy"
 # def tell_me_about_the_octopus(self):
 #         print("This octopus is " + self.color + ".")
 #         print(self.name + " is the octopus's name.")
+
+
+# # Define a function
+# def world():
+#     print("Hai, Semuanya!")
+
+# # Call function within module
+# world()
